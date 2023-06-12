f8b6c6f7c8b9e10706d9b419bdf5a05f515a16f9
Oussama <obr.oussama.rejeb@gmail.com>
2023-06-12 15:43:21 +0200

    add Pizza model

--- api/models.py
+++ api/models.py
@@ -5,3 +5,9 @@ class Ingredients(models.Model):
     name = models.CharField(max_length=200)
 
 
+class Pizza(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=2)
+    ingredients = models.ManyToManyField(Ingredients)
+
+
